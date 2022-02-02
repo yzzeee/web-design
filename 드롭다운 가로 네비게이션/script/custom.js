6156cd3ed84ef31a59ca75f52df1534afa7d34d8
yzzeee <goooooodgood@gmail.com>
2022-02-02 16:34:59 +0900

    드롭다운 가로 네비게이션 타입 3

--- 드롭다운 가로 네비게이션/script/custom.js
+++ 드롭다운 가로 네비게이션/script/custom.js
@@ -23,9 +23,9 @@ $('.close-modal').on('click', function() {
 
 /* Navigation */
 $('.menu li').mouseenter(function(){
-  $('.sub-menu').stop().slideDown();
+  $('.sub-menu, .sub-back').stop().slideDown();
 });
 
 $('.menu li').mouseleave(function(){
-  $('.sub-menu').stop().slideUp();
+  $('.sub-menu, .sub-back').stop().slideUp();
 });
